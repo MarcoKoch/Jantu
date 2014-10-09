cf4c44082da44af141a6d4953bae6ef2ba69aecb
Marco Koch <marco-koch@t-online.de>
2014-10-09 14:23:56 +0200

    Erster Commit für das Entity-System

--- Jantu_Main/Jantu_Main/Program.cs
+++ Jantu_Main/Jantu_Main/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Jantu_Main
+﻿namespace Jantu
 {
     class Program
     {
