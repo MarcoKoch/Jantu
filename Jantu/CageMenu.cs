fe5ff74e5218d0314561a7af0d9d3029e6661516
DarkLiora <miriamkaisers@t-online.de>
2014-11-15 18:39:10 +0100

    Nachricht

--- Jantu/CageMenu.cs
+++ Jantu/CageMenu.cs
@@ -7,14 +7,14 @@ namespace Jantu
 {
     class CageMenu
     {
-        public CageMenu(int originX, int originY, int width, int height, Game game, UI ui)
+        public CageMenu(int originX, int originY, int width, int height, Game game/* ,UI ui */)
         {
             _originX = originX;
             _originY = originY;
             _width = width;
             _height = height;
             _game = game;
-            _ui = ui;
+            //_ui = ui;
 
 
         }
@@ -24,7 +24,7 @@ namespace Jantu
     private int _width;
     private int _height;
     private Game _game;
-    private UI _ui;
+   // private UI _ui;
     }
 
    
