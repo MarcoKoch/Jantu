aae0e342668a59ea893738a37ef0a299548a17a4
Typhoniel <syviasilverleaf@gmail.com>
2015-02-12 17:45:13 +0100

    Kläglicher Try noch ein paar schlaue Gedanken zu haben

--- Jantu/ActionMenu.cs
+++ Jantu/ActionMenu.cs
@@ -7,8 +7,21 @@ namespace Jantu
 {
     class ActionMenu : Window
     {
+        private int _Width;
+        private int _Height;
+        private Vector2 _Pos;
+
         public ActionMenu(Vector2 pos, int width, int height) :
                 base(pos, width, height)
-        {}
+        {
+            _Width = width;
+            _Height = height;
+            _Pos = pos;
+        }
+
+        protected override void OnDraw()
+        {
+            return;
+        }
     }
 }
